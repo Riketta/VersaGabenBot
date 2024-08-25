85ff8b7eb04fc98079444cfd63f8a60167ce1542
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-08-25 22:54:49 +0300

    Add initial Config implementation.

---
+++ VersaGabenBot/Config.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using VersaGabenBot.Options;
+
+namespace VersaGabenBot
+{
+    internal class Config
+    {
+        public static readonly string DefaultConfigPath = $"{nameof(VersaGabenBot)}.json";
+
+        /// <summary>
+        /// Path associated with the current config instance.
+        /// </summary>
+        [JsonIgnore]
+        public string PathToConfig { get; private set; }
+
+        #region Config Fields
+        public BotConfig BotConfig { get; set; } = new BotConfig();
+        #endregion
+
+        [JsonConstructor]
+        private Config()
+        {
+            PathToConfig = DefaultConfigPath;
+        }
+
+        private Config(string pathToConfig)
+        {
+            if (string.IsNullOrEmpty(pathToConfig))
+                throw new ArgumentNullException(nameof(pathToConfig));
+
+            PathToConfig = pathToConfig;
+        }
+
+        ~Config()
+        {
+        }
+
+        public static Config LoadOrCreateDefault(string pathToConfig)
+        {
+            if (string.IsNullOrEmpty(pathToConfig))
+                throw new ArgumentNullException(nameof(pathToConfig));
+
+            if (!File.Exists(pathToConfig))
+                return SaveDefault(pathToConfig);
+
+            return Load(pathToConfig);
+        }
+
+        public static Config Load(string pathToConfig)
+        {
+            if (string.IsNullOrEmpty(pathToConfig))
+                throw new ArgumentNullException(nameof(pathToConfig));
+
+            Config config;
+            if (!File.Exists(pathToConfig))
+                throw new ArgumentException($"No config file found: \"{pathToConfig}\"!");
+
+            string json = File.ReadAllText(pathToConfig);
+            config = JsonConvert.DeserializeObject<Config>(json) ?? throw new InvalidOperationException();
+            config.PathToConfig = pathToConfig;
+
+            // TODO: initialize default values, migrations and overrides here if required.
+
+            config.Save(); // Re-save to add new or missing config fields.
+
+            return config;
+        }
+
+        public static Config SaveDefault()
+        {
+            return SaveDefault(DefaultConfigPath);
+        }
+
+        public static Config SaveDefault(string pathToConfig)
+        {
+            Config config = new Config(pathToConfig);
+            config.Save();
+
+            return config;
+        }
+
+        public Config Save()
+        {
+            string json = ToJson();
+            File.WriteAllText(PathToConfig, json);
+
+            return this;
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, new JsonSerializerSettings() { Formatting = Formatting.Indented, });
+        }
+    }
+}
---
+++ VersaGabenBot/Options/BotConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersaGabenBot.Options
+{
+    public class BotConfig
+    {
+        public string Token { get; set; }
+        public ulong GuildID { get; set; }
+        public ulong GeneralChannelID { get; set; }
+        public ulong BotChannelID { get; set; }
+        public uint StatusUpdateInterval { get; set; } = 60 * 60 * 1000;
+        public List<string> StatusList { get; set; }
+    }
+}
