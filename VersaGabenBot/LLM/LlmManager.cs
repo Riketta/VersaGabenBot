a67b8a7dd3ff165c47df0393710f88e8619095a6
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-08-27 17:25:01 +0300

    Add message prefix with user ID for LLM.

--- VersaGabenBot/LLM/LlmManager.cs
+++ VersaGabenBot/LLM/LlmManager.cs
@@ -24,9 +24,10 @@ namespace VersaGabenBot.LLM
 
         public async Task ProcessMessageAsync(SocketSelfUser currentUser, SocketUserMessage message, Guild guild)
         {
+            Message llmMessage = new Message(Roles.User, $"{message.Author.Mention}: {message}");
             // Store messages in history that not even addressed to bot.
             if (!_options.OnlySaveChatHistoryRelatedToBot)
-                guild.AppendMessage(message.Channel.Id, new Message(Roles.User, message));
+                guild.AppendMessage(message.Channel.Id, llmMessage);
 
             bool isRandomReply = new Random().NextDouble() <= _options.RandomReplyChance;
             bool isMentioned = message.MentionedUsers.Any(user => user.Id == currentUser.Id);
@@ -37,7 +38,7 @@ namespace VersaGabenBot.LLM
             using IDisposable typing = message.Channel.EnterTypingState();
 
             if (_options.OnlySaveChatHistoryRelatedToBot)
-                guild.AppendMessage(message.Channel.Id, new Message(Roles.User, message));
+                guild.AppendMessage(message.Channel.Id, llmMessage);
             Message response = await _client.GenerateTextAsync(guild.MessageHistoryPerChannel[message.Channel.Id].ToArray());
             guild.AppendMessage(message.Channel.Id, response);
 
--- VersaGabenBot/Options/BotConfig.cs
+++ VersaGabenBot/Options/BotConfig.cs
@@ -9,6 +9,7 @@ namespace VersaGabenBot.Options
     public class BotConfig : IOptions
     {
         public string Token { get; set; }
+        public ulong SelfID { get; set; }
         public uint StatusUpdateInterval { get; set; } = 60 * 60 * 1000;
         public List<string> StatusList { get; set; }
     }
