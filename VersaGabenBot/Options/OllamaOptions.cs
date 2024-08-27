af48090b0b195b48c880493f57ce3c7dc38a5b76
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-08-27 05:09:47 +0300

    Increase default HttpClientTimeout to 210 seconds.

--- VersaGabenBot/Options/OllamaOptions.cs
+++ VersaGabenBot/Options/OllamaOptions.cs
@@ -10,7 +10,7 @@ namespace VersaGabenBot.Options
     internal class OllamaOptions : IOptions
     {
         public string OllamaApiUrl { get; set; } = "http://localhost:11434/api/";
-        public int HttpClientTimeout { get; set; } = 200;
+        public int HttpClientTimeout { get; set; } = 210;
         public string Model { get; set; } = "phi3.5";
         public bool Multimodal { get; set; } = false;
         public uint ContextWindow { get; set; } = 16384;
