7d3ca22ce26ef1cb3f329b6150dd8a46b1f83234
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-09-05 23:53:08 +0300

    LlmOptionsGuild now implements IOptions.

--- VersaGabenBot/Options/LlmOptionsGuild.cs
+++ VersaGabenBot/Options/LlmOptionsGuild.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace VersaGabenBot.Options
 {
-    internal class LlmOptionsGuild
+    internal class LlmOptionsGuild : IOptions
     {
         public bool OnlySaveChatHistoryRelatedToBot { get; set; } = false;
         public double RandomReplyChance { get; set; } = 0.01f;
