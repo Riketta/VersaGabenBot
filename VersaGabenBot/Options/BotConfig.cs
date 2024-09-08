b91a8219459f17b05c6f7e6d202266e90c972a2c
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-09-09 02:39:06 +0300

    Add BotConfig.MaxMessageLength.

--- VersaGabenBot/Options/BotConfig.cs
+++ VersaGabenBot/Options/BotConfig.cs
@@ -9,6 +9,7 @@ namespace VersaGabenBot.Options
     public class BotConfig : IOptions
     {
         public string Token { get; set; }
+        public uint MaxMessageLength { get; set; } = 2000;
         public uint StatusUpdateInterval { get; set; } = 60 * 60 * 1000;
         public List<string> StatusList { get; set; }
     }
