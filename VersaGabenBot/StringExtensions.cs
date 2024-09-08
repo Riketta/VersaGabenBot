152bfdb4933dee61bfa38631c8371912ba9f39b8
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-09-09 02:38:26 +0300

    Add StringExtensions.SplitByLengthAtNewLine.

---
+++ VersaGabenBot/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersaGabenBot
+{
+    internal static class StringExtensions
+    {
+        public static string[] SplitByLengthAtNewLine(this string str, int length)
+        {
+            List<string> result = new List<string>();
+            StringBuilder sb = new StringBuilder(length);
+            int currentLength = 0;
+
+            foreach (string line in str.Split('\n'))
+            {
+                if (currentLength + line.Length > length)
+                {
+                    result.Add(sb.ToString());
+                    sb.Clear();
+                    currentLength = 0;
+                }
+
+                currentLength += line.Length;
+                sb.Append(line).Append('\n');
+            }
+
+            return result.ToArray();
+        }
+    }
+}
