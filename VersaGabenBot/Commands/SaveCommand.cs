7416964d4e54866d35b0c4c3b8d04853b36243a3
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-09-11 04:34:01 +0300

    Add templates for WipeCommand and SaveCommand.

---
+++ VersaGabenBot/Commands/SaveCommand.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.Rest;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersaGabenBot.Commands
+{
+    internal class SaveCommand : IGlobalCommand
+    {
+        public RestGlobalCommand RestGlobalCommand { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string Name => throw new NotImplementedException();
+
+        public RestApplicationCommand RestApplicationCommand => throw new NotImplementedException();
+
+        public Task Handle(SocketSlashCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SlashCommandProperties Prepare()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
