e14da16c606efbde0a73d25ad9bc47a1226e4c95
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-09-13 04:06:42 +0300

    Add BotConfig.DebugUserID.

--- VersaGabenBot/Options/BotConfig.cs
+++ VersaGabenBot/Options/BotConfig.cs
@@ -9,6 +9,7 @@ namespace VersaGabenBot.Options
     public class BotConfig : IOptions
     {
         public string Token { get; set; }
+        public ulong DebugUserID { get; set; }
         public int MaxMessageLength { get; set; } = 2000;
         public uint StatusUpdateInterval { get; set; } = 60 * 60 * 1000;
         public List<string> StatusList { get; set; }
