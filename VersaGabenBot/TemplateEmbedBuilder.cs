28530175148b7f22b21a32b082d53d895f8385d7
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-09-13 04:20:39 +0300

    Add template embed builder.

---
+++ VersaGabenBot/TemplateEmbedBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VersaGabenBot.Data.Models;
+
+namespace VersaGabenBot
+{
+    internal class TemplateEmbedBuilder
+    {
+        public static EmbedBuilder Error(string message)
+        {
+            var embed = new EmbedBuilder()
+            .WithTitle("Error")
+            .WithDescription(message)
+            .WithColor(Color.Red)
+            .WithCurrentTimestamp();
+
+            return embed;
+        }
+
+        public static EmbedBuilder ErrorUnauthorizedCommandUse()
+        {
+            return Error("Unauthorized use of command!");
+        }
+    }
+}
