2c8acc10b4375c4dcf28b78ea8efe753d4535e30
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-09-15 20:50:37 +0300

    Initial tests setup for repositories.

---
+++ VersaGabenBot.Tests/Repositories/ChannelRepositoryTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersaGabenBot.Tests.Repositories
+{
+    internal class ChannelRepositoryTests
+    {
+
+    }
+}
--- VersaGabenBot.Tests/Repositories/GuildRepositoryTests.cs
+++ VersaGabenBot.Tests/Repositories/GuildRepositoryTests.cs
@@ -1,11 +1,40 @@
+using VersaGabenBot.Data.Models;
+using VersaGabenBot.Data.Repositories;
+using VersaGabenBot.Tests.Mocks;
+
 namespace VersaGabenBot.Tests.Repositories
 {
     [TestClass]
     public class GuildRepositoryTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private readonly MockDatabaseContext _db = new MockDatabaseContext();
+        private GuildRepository _guildRepository;
+
+        [TestInitialize]
+        public void Setup()
         {
+            _db.Wipe();
+            new DatabaseInitializer(_db).Initialize();
+            _guildRepository = new GuildRepository(_db);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+        }
+
+        [TestMethod]
+        public void TestRegisterGuild()
+        {
+            ulong guildId = 1525;
+
+            Guild guild = _guildRepository.RegisterGuild(guildId).Result;
+            Guild sameGuild = _guildRepository.GetGuild(guildId).Result;
+
+            Assert.AreEqual(guild.GuildID, guildId);
+            Assert.AreEqual(guild.GuildID, sameGuild.GuildID);
+            Assert.AreEqual(guild.SystemChannelID, sameGuild.SystemChannelID);
+            Assert.AreEqual(guild.Channels?.Count, guild.Channels?.Count);
         }
     }
 }
